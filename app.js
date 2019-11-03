083a9eeabcbccd514cd6b4cf801ed38474bc9710
Zsombor99 <55382782+Zsombor99@users.noreply.github.com>
2019-11-03 22:01:08 +0100

    renderHtml +app.js changes
    még mindíg nem működik

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const router = express.Router();
+var fs = require('fs');
+
+var data = fs.readFileSync('./files/data.txt','utf8');
+
+var jsonStrig = '[';
+var items = data.split('$$$');
+for (var i = 0; i < items.length; i++) {  
+  var current = items[i].split('/n');
+  jsonStrig += '{"' + 'title' + '":"' + current[i+3] + '",' +
+    '"' + 'originalLabels' + '":"' + current[i+2] + '",'+
+    '"' + 'recommandedLabels'+ '":"' + current[i+0] + '",' +
+    '"' + 'alsoRecommandedLabels' + '":"' + current[i+1] + '",' + 
+    '"' + 'text' +  '":"' +  current[i+4] + '"},';
+}
+jsonStrig = jsonStrig.substr(0, jsonStrig.length - 1);
+jsonStrig += ']';
+
+var obj = JSON.parse(jsonStrig);
+
 
 app.use('/css', express.static('css'));
 app.use('/js', express.static('js'));
+app.use(express.static(__dirname + '/views'));
 
 router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/views/index.html'));
+  res.render('index', { data: JSON.stringify(obj) })
   //__dirname : It will resolve to your project folder.
 });
-
 
 
 //add the router
