45ddc2a37336b499b6b234e967bf1d8ebb08f801
Pauli Gergő <gergopauli2@gmial.com>
2019-12-03 23:31:46 +0100

    Project 2 - start
    Signed-off-by: Pauli Gergő <gergopauli2@gmial.com>

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const config = require('./db');
+
+mongoose.connect(config.DB, {useNewUrlParser: true}).then(
+    () => {
+        console.log('Database is connected')
+    },
+    err => {
+        console.log('Can not connect to the database' + err)
+    }
+);
 
 const app = express();
 
