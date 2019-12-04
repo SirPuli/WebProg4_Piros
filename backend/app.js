557926a37c0234c2deb7ede469d6ec25014fd2c7
puli96 <Qrp135rt8D>
2019-12-04 12:01:01 +0100

    login, register

--- backend/app.js
+++ backend/app.js
@@ -4,21 +4,23 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const config = require('./db');
 
-mongoose.connect(config.DB, {useNewUrlParser: true}).then(
-    () => {
-        console.log('Database is connected')
-    },
-    err => {
-        console.log('Can not connect to the database' + err)
-    }
+const users = require('./routes/user');
+
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+    () => {console.log('Database is connected') },
+    err => { console.log('Can not connect to the database'+ err)}
 );
 
 const app = express();
+app.use(passport.initialize());
+require('./passport')(passport);
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
+app.use('/api/users', users);
+
+app.get('/', function(req, res) {
     res.send('hello');
 });
 
