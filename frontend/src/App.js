15072b7080f0e7eac8ccc247ab59c5ca532252e1
Zsombor99 <55382782+Zsombor99@users.noreply.github.com>
2019-12-11 22:47:15 +0100

    adatbazis kapcsolat + hiba javítás

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,12 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+// App.js
 
-import Navbar from './components/Navbar';
-import Register from './components/Register';
-import Login from './components/Login';
-import Home from './components/Home';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-class App extends Component {
-    render() {
-        return (
-            <Router>
-                <div>
-                    <Navbar />
-                    <Route exact path="/" component={ Home } />
-                    <div className="container">
-                        <Route exact path="/register" component={ Register } />
-                        <Route exact path="/login" component={ Login } />
-                    </div>
-                </div>
-            </Router>
-        );
-    }
-}
-
-export default App;
-
-//ennek kéne itt lennie csak meghal töle
-//valszeg a store.js a baj
-
-/*
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authentication';
 
 import Navbar from './components/Navbar';
 import Register from './components/Register';
@@ -42,6 +14,18 @@ import Login from './components/Login';
 import Home from './components/Home';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+if(localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decoded));
+
+  const currentTime = Date.now() / 1000;
+  if(decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+    window.location.href = '/login'
+  }
+}
 
 class App extends Component {
   render() {
@@ -63,5 +47,3 @@ class App extends Component {
 }
 
 export default App;
-
- */
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,4 +1,10 @@
+// Login.js
+
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loginUser } from '../actions/authentication';
+import classnames from 'classnames';
 
 class Login extends Component {
 
@@ -25,43 +31,78 @@ class Login extends Component {
             email: this.state.email,
             password: this.state.password,
         }
-        console.log(user);
+        this.props.loginUser(user);
+    }
+
+    componentDidMount() {
+        if(this.props.auth.isAuthenticated) {
+            this.props.history.push('/');
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.auth.isAuthenticated) {
+            this.props.history.push('/')
+        }
+        if(nextProps.errors) {
+            this.setState({
+                errors: nextProps.errors
+            });
+        }
     }
 
     render() {
+        const {errors} = this.state;
         return(
-            <div className="container" style={{ marginTop: '50px', width: '700px'}}>
-                <h2 style={{marginBottom: '40px'}}>Bejelentkezés</h2>
-                <form onSubmit={ this.handleSubmit }>
-                    <div className="form-group">
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            className="form-control"
-                            name="email"
-                            onChange={ this.handleInputChange }
-                            value={ this.state.email }
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input
-                            type="password"
-                            placeholder="Jelszó"
-                            className="form-control"
-                            name="password"
-                            onChange={ this.handleInputChange }
-                            value={ this.state.password }
-                        />
-                    </div>
-                    <div className="form-group">
-                        <button type="submit" className="btn btn-primary">
-                            Bejelentkezés
-                        </button>
-                    </div>
-                </form>
-            </div>
+        <div className="container" style={{ marginTop: '50px', width: '700px'}}>
+            <h2 style={{marginBottom: '40px'}}>Login</h2>
+            <form onSubmit={ this.handleSubmit }>
+                <div className="form-group">
+                    <input
+                    type="email"
+                    placeholder="Email"
+                    className={classnames('form-control form-control-lg', {
+                        'is-invalid': errors.email
+                    })}
+                    name="email"
+                    onChange={ this.handleInputChange }
+                    value={ this.state.email }
+                    />
+                    {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
+                </div>
+                <div className="form-group">
+                    <input
+                    type="password"
+                    placeholder="Password"
+                    className={classnames('form-control form-control-lg', {
+                        'is-invalid': errors.password
+                    })} 
+                    name="password"
+                    onChange={ this.handleInputChange }
+                    value={ this.state.password }
+                    />
+                    {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
+                </div>
+                <div className="form-group">
+                    <button type="submit" className="btn btn-primary">
+                        Login User
+                    </button>
+                </div>
+            </form>
+        </div>
         )
     }
 }
 
-export default Login;
+Login.propTypes = {
+    loginUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+    errors: state.errors
+})
+
+export  default connect(mapStateToProps, { loginUser })(Login)
